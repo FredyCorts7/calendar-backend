65716d2187316e1609a84a3a1b5f02ac932c21d1
fredycorts7 <fred.cor.14@gmail.com>
2020-09-21 18:06:07 -0500

    <ADD> Validando la restriccion unique establecida en el modelo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,24 @@
 const User = require('../models/User');
 
 const addUser = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    //   const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
 
-    const user = new User(req.body);
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User already exists',
+      });
+    }
+
+    user = new User(req.body);
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: 'Register endpoint',
+      uid: user.id,
+      name: user.name,
     });
   } catch (error) {
     res.status(500).json({
