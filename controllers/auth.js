61469a16357883c3c3a007c6c08226c202b3b038
fredycorts7 <fred.cor.14@gmail.com>
2020-09-21 18:28:25 -0500

    <ADD> Implementando logica de login

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,13 +34,38 @@ const addUser = async (req, res) => {
   }
 };
 
-const getUser = (req, res) => {
+const getUser = async (req, res) => {
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: 'Log In endpoint',
-  });
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Credentials invalid',
+      });
+    }
+
+    const validPasswords = bcrypt.compareSync(password, user.password);
+    if (!validPasswords) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Credentials invalid',
+      });
+    }
+
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Error of server',
+    });
+  }
 };
 
 const refreshToken = (req, res) => {
